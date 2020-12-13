e457876ebb4bc02e540bb6db40c881401b113502
Yusuf Emirhan Şahin <syusufemirhan@gmail.com>
2020-12-13 21:40:53 +0300

    Login screen styling finished

--- lib/screens/authenticate/Register.dart
+++ lib/screens/authenticate/Register.dart
@@ -20,63 +20,89 @@ class _RegisterState extends State<Register> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 3.0,
-        title: Text("Kaydol"),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Giriş Yap'),
-            onPressed: () => widget.toggleView(),
-          ),
-        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text(
+          "Üye Ol",
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.keyboard_backspace),
+          color: Colors.black,
+          onPressed: () => Navigator.pop(context),
+        ),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (value) =>
-                    value.isEmpty ? "Bir E-Posta Adresi Giriniz" : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (value) => value.length < 7
-                    ? "7 karakterden uzun bir şifre giriniz"
-                    : null,
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Theme.of(context).primaryColor,
-                child: Text(
-                  'Kaydol',
-                  style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        clipBehavior: Clip.none,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 60.0),
+                SizedBox(height: 20.0),
+                Text(
+                  "Toplulugumuzun bir parçası ol!",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Lato"),
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result = _auth.register(email, password);
-                    if (result == null) {
-                      setState(() => error = "Doğru bir e posta verin");
+                SizedBox(height: 30.0),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "E-Posta"),
+                  validator: (value) =>
+                      value.isEmpty ? "Bir E-Posta Adresi Giriniz" : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Şifre"),
+                  validator: (value) => value.length < 7
+                      ? "7 karakterden uzun bir şifre giriniz"
+                      : null,
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                SizedBox(height: 30.0),
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)),
+                  child: Container(
+                      width: 120,
+                      height: 40,
+                      child: Center(
+                        child: new Text(
+                          'Üye Ol',
+                          style: new TextStyle(
+                              fontSize: 18.0, color: Colors.white),
+                        ),
+                      )),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = _auth.register(email, password);
+                      if (result == null) {
+                        setState(
+                            () => error = "Lütfen Doğru Bir E-Posta Giriniz!");
+                      }
                     }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
+                  },
+                ),
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14.0),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/authenticate/SignIn.dart
+++ lib/screens/authenticate/SignIn.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kablosuzbeyin/authentication/AuthService.dart';
+import 'package:kablosuzbeyin/screens/authenticate/Register.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -19,64 +20,98 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 3.0,
-        title: Text("Giriş Yap"),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Kaydol'),
-            onPressed: () => widget.toggleView(),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (value) =>
-                    value.isEmpty ? "Bir E-Posta Adresi Giriniz" : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (value) => value.length < 7
-                    ? "7 karakterden uzun bir şifre giriniz"
-                    : null,
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Theme.of(context).primaryColor,
-                child: Text(
-                  'Giriş Yap',
-                  style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        clipBehavior: Clip.none,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 60.0),
+                Image.asset(
+                  "assets/images/logo.png",
+                  width: 250,
+                  height: 250,
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result = _auth.signIn(email, password);
-                    if (result == null) {
-                      setState(() => error = "Yanlış kullanıcı adı/şifre");
+                SizedBox(height: 20.0),
+                Text(
+                  "Kablosuz Beyin",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Lato"),
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "E-Posta"),
+                  validator: (value) =>
+                      value.isEmpty ? "Bir E-Posta Adresi Giriniz" : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Şifre"),
+                  validator: (value) => value.length < 7
+                      ? "7 karakterden uzun bir şifre giriniz"
+                      : null,
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)),
+                  child: Container(
+                      width: 120,
+                      height: 45,
+                      child: Center(
+                        child: new Text(
+                          'Giriş Yap',
+                          style: new TextStyle(
+                              fontSize: 20.0, color: Colors.white),
+                        ),
+                      )),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = _auth.signIn(email, password);
+                      if (result == null) {
+                        setState(() => error = "Yanlış kullanıcı adı/şifre");
+                      }
                     }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
+                  },
+                ),
+                SizedBox(height: 20.0),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Register()),
+                    );
+                  },
+                  child: Container(
+                    child: Text(
+                      "Üye Ol",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Text("Üye Olmadan devam et"),
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14.0),
+                )
+              ],
+            ),
           ),
         ),
       ),
